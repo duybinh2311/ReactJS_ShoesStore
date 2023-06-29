e6e602b8acb558a60b7823da80c0ee6691d39008
duybinh2311 <duybinh.nguyen2311@gmail.com>
2023-06-29 07:49:54 +0700

    update check login cart page

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,17 +1,33 @@
 import { Container, Grid, Text, Title } from '@mantine/core'
 import useNaviProgress from 'hooks/useNaviProgress'
-import React from 'react'
+import React, { useEffect } from 'react'
 import useStyles from './CartPage.style'
 import CartList from './cartList/CartList'
 import OrderDetail from './orderDetail/OrderDetail'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-hot-toast'
+import openLogin from 'components/formLogin/openLogin'
 
 export default function CartPage() {
   useNaviProgress()
   /* App State */
+  const { userLogin } = useSelector((state) => state.user)
   const { totalItem } = useSelector((state) => state.cart)
+  /* Hook Init */
+  const navigate = useNavigate()
   /* Style */
   const { classes } = useStyles()
+  /* Logic */
+  useEffect(() => {
+    if (!userLogin.email) {
+      navigate('/')
+      return () => {
+        toast.error('You are not logged in')
+        openLogin()
+      }
+    }
+  }, [userLogin])
   return (
     <>
       <section className={classes.cart}>
